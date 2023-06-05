a07c7b4a3bb51a3791c5d0c3336736e394295ca7
Hendrik Mennen <hmennen@protop-solutions.com>
2023-06-05 15:13:56 +0200

    .

--- src/OneWare.Cpp/CppModule.cs
+++ src/OneWare.Cpp/CppModule.cs
@@ -13,7 +13,7 @@ public class CppModule : IModule
 
     public void OnInitialized(IContainerProvider containerProvider)
     {
-        containerProvider.Resolve<ILanguageManager>().RegisterHighlighting("Assets/CPP-Mode.xshd", ".cpp", ".h", ".c", ".hpp");
+        containerProvider.Resolve<ILanguageManager>().RegisterHighlighting("avares://OneWare.Cpp/Assets/CPP-Mode.xshd", ".cpp", ".h", ".c", ".hpp");
         containerProvider.Resolve<ILanguageManager>().RegisterService(typeof(LanguageServiceCpp),false, ".cpp", ".h", ".c", ".hpp");
     }
 }
