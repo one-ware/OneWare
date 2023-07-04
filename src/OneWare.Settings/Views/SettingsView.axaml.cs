fd4e6be11d0426d58a7cc3a05b9bf7bc9f4943c8
Hendrik Mennen <hmennen@protop-solutions.com>
2023-07-04 17:11:28 +0200

    style fixes

--- src/OneWare.Settings/Views/SettingsView.axaml.cs
+++ src/OneWare.Settings/Views/SettingsView.axaml.cs
@@ -1,4 +1,8 @@
-﻿using Avalonia;
+﻿using System.Reactive.Linq;
+using Avalonia;
+using Avalonia.Threading;
+using DynamicData.Binding;
+using OneWare.Settings.Models;
 using OneWare.Shared;
 
 namespace OneWare.Settings.Views
@@ -8,6 +12,20 @@ namespace OneWare.Settings.Views
         public SettingsView()
         {
             InitializeComponent();
+            
+            TreeView.WhenValueChanged(x => x.SelectedItem)
+                .Throttle(TimeSpan.FromMilliseconds(50)).Subscribe(x =>
+            {
+                if (x is SubCategoryModel)
+                {
+                    Dispatcher.UIThread.Post(() =>
+                    {
+                        SettingsList.ScrollIntoView(x);
+                        SettingsList.ContainerFromItem(x)?.Classes.Remove("Highlight");
+                        SettingsList.ContainerFromItem(x)?.Classes.Add("Highlight");
+                    });
+                }
+            });
         }
     }
 }
