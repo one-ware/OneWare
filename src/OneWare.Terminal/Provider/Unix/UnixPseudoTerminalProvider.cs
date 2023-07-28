3e095ad5422141f8f23bf811adab1cd0f6f199cd
Hendrik Mennen <hmennen@protop-solutions.com>
2023-07-28 09:23:51 +0200

    fix terminal for linux net7.0

--- src/OneWare.Terminal/Provider/Unix/UnixPseudoTerminalProvider.cs
+++ src/OneWare.Terminal/Provider/Unix/UnixPseudoTerminalProvider.cs
@@ -17,9 +17,21 @@ namespace OneWare.Terminal.Provider.Unix
             var name = Marshal.PtrToStringAnsi(namePtr);
             if (name == null) throw new NullReferenceException(nameof(name));
 
+            //Collect ENV Vars before to avoid EntryPointNotFoundException
+            var envVars = new List<string>();
+            var env = Environment.GetEnvironmentVariables();
+            foreach (var variable in env.Keys) 
+            {
+                if (variable.ToString() != "TERM") {
+                    envVars.Add($"{variable}={env[variable]}");
+                }
+            }
+            envVars.Add("TERM=xterm-256color");
+            envVars.Add(null!);
+
             //Duplicate current process
             var pid = Native.fork(); 
-
+            
             //Newly created child process will have PID = 0
             if (pid == 0)
             {
@@ -28,24 +40,11 @@ namespace OneWare.Terminal.Provider.Unix
                 Native.dup2(slave, 0);
                 Native.dup2(slave, 1);
                 Native.dup2(slave, 2);
-                
+
                 Native.setsid();
                 Native.ioctl(slave, Native.TIOCSCTTY, IntPtr.Zero);
                 Native.chdir(initialDirectory);
-
-                var envVars = new List<string>();
-                var env = Environment.GetEnvironmentVariables();
-
-                foreach (var variable in env.Keys) 
-                {
-                    if (variable.ToString() != "TERM") {
-                        envVars.Add($"{variable}={env[variable]}");
-                    }
-                }
                 
-                envVars.Add("TERM=xterm-256color");
-                envVars.Add(null!);
-
                 var argsArray = new List<string> { "/bin/bash" };
                 if(arguments?.Length > 0 && !string.IsNullOrEmpty(arguments[0])) 
                     argsArray.AddRange(arguments);
@@ -54,7 +53,7 @@ namespace OneWare.Terminal.Provider.Unix
 
                 Native.execve(argsArray[0], argsArray.ToArray(), envVars.ToArray());
             }
-
+            
             var stdin = Native.dup(fdm);
             var process = Process.GetProcessById(pid);
             return new UnixPseudoTerminal(process, stdin, new FileStream(new SafeFileHandle(new IntPtr(stdin), true), 
