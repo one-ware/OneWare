08e83434897cde7aa25eb1a1d6b0d472a1fab917
Hendrik Mennen <hmennen@protop-solutions.com>
2023-11-20 15:38:15 +0100

    net 8 browser fixes

--- demo/OneWare.Demo.Browser/AppBundle/main.js
+++ demo/OneWare.Demo.Browser/AppBundle/main.js
@@ -1,4 +1,8 @@
-﻿import { dotnet } from './dotnet.js'
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+//import { dotnet } from './dotnet.js' // NET 7
+import { dotnet } from './_framework/dotnet.js' // NET 8+
 
 const is_browser = typeof window != "undefined";
 if (!is_browser) throw new Error(`Expected to be running in a browser`);
@@ -10,4 +14,4 @@ const dotnetRuntime = await dotnet
 
 const config = dotnetRuntime.getConfig();
 
-await dotnetRuntime.runMainAndExit(config.mainAssemblyName, [window.location.search]);
+await dotnetRuntime.runMainAndExit(config.mainAssemblyName, [globalThis.location.href]);
