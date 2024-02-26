c07d16636e7ca9efc1bf232123aa909ea7d05084
Hendrik Mennen <hmennen@protop-solutions.com>
2024-02-26 09:46:35 +0100

    add verible lsps

--- src/OneWare.Verilog/VerilogModule.cs
+++ src/OneWare.Verilog/VerilogModule.cs
@@ -23,8 +23,16 @@ public class VerilogModule : IModule
         
         var nativeTool = nativeToolService.Register(LspName);
             
-        nativeTool.AddPlatform(PlatformId.WinX64, "https://github.com/chipsalliance/verible/releases/download/v0.0-3430-g060bde0f/verible-v0.0-3430-g060bde0f-win64.zip")
-            .WithShortcut("LSP", Path.Combine("verible-v0.0-3430-g060bde0f-win64", "verible-verilog-ls.exe"),
+        nativeTool.AddPlatform(PlatformId.WinX64, "https://github.com/chipsalliance/verible/releases/download/v0.0-3582-g25611a89/verible-v0.0-3582-g25611a89-win64.zip")
+            .WithShortcut("LSP", Path.Combine("verible-v0.0-3582-g25611a89-win64", "verible-verilog-ls.exe"),
+                LspPathSetting);
+        
+        nativeTool.AddPlatform(PlatformId.LinuxX64, "https://github.com/chipsalliance/verible/releases/download/v0.0-3582-g25611a89/verible-v0.0-3582-g25611a89-linux-static-x86_64.tar.gz")
+            .WithShortcut("LSP", Path.Combine("verible-v0.0-3582-g25611a89", "bin", "verible-verilog-ls"),
+                LspPathSetting);
+        
+        nativeTool.AddPlatform(PlatformId.OsxArm64, "https://github.com/chipsalliance/verible/releases/download/v0.0-3582-g25611a89/verible-v0.0-3582-g25611a89-macOS.tar.gz")
+            .WithShortcut("LSP", Path.Combine("verible-v0.0-3582-g25611a89", "bin", "verible-verilog-ls"),
                 LspPathSetting);
 
         containerProvider.Resolve<ISettingsService>().RegisterTitledPath("Languages", "Verilog", LspPathSetting,
