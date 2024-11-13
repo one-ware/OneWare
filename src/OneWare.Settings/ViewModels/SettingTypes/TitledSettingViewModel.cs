a17f6fec5d9663094a2ea16c0ebd24aeb86081f2
Hendrik Mennen <hmennen@protop-solutions.com>
2024-11-13 12:45:38 -0500

    version

--- src/OneWare.Settings/ViewModels/SettingTypes/TitledSettingViewModel.cs
+++ src/OneWare.Settings/ViewModels/SettingTypes/TitledSettingViewModel.cs
@@ -4,8 +4,6 @@ namespace OneWare.Settings.ViewModels.SettingTypes;
 
 public abstract class TitledSettingViewModel : SettingViewModel
 {
-    private bool _isEnabled = true;
-
     protected TitledSettingViewModel(TitledSetting setting)
     {
         Setting = setting;
