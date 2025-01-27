c81a45346eb9554dd39fdfb7f4c212e98dc8f547
Hendrik Mennen <hmennen@protop-solutions.com>
2025-01-27 18:44:53 -0400

    fix window dialogs

--- src/OneWare.Core/Services/WindowService.cs
+++ src/OneWare.Core/Services/WindowService.cs
@@ -109,8 +109,7 @@ public class WindowService : IWindowService
             owner ??= ContainerLocator.Container.Resolve<MainWindow>();
             await window.ShowDialogAsync(owner);
         }
-
-        await ContainerLocator.Container.Resolve<MainView>().ShowVirtualDialogAsync(window);
+        else await ContainerLocator.Container.Resolve<MainView>().ShowVirtualDialogAsync(window);
         
         window.Focus();
     }
