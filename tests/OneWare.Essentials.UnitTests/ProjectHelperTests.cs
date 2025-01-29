37a5beda5a40e4b6c785af16c5fa53df07227547
Hendrik Mennen <hmennen@protop-solutions.com>
2025-01-28 21:10:04 -0400

    tests

--- tests/OneWare.Essentials.UnitTests/ProjectHelperTests.cs
+++ tests/OneWare.Essentials.UnitTests/ProjectHelperTests.cs
@@ -1,18 +1,38 @@
-﻿using OneWare.Essentials.Helpers;
+﻿﻿using OneWare.Essentials.Helpers;
 using Xunit;
 
 namespace OneWare.Essentials.UnitTests;
 
 public class ProjectHelperTests
 {
-    [Fact]
-    public void MathWildCardTests()
-    {
-        var include = new[] { "**" };
-        var exclude = new[] { ".git" };
+[Fact]
+public void MathWildCardTests()
+{
+    var include = new[] { "**" };
+    var exclude = new[] { ".git" };
 
-        Assert.False(ProjectHelper.MatchWildCards(".git/FETCH_HEAD", include, exclude));
-        Assert.True(ProjectHelper.MatchWildCards("test.vhd", include, exclude));
-        Assert.True(ProjectHelper.MatchWildCards("nice/test.vhd", include, exclude));
-    }
+    // Test cases for .git directory
+    Assert.False(ProjectHelper.MatchWildCards(".git/FETCH_HEAD", include, exclude));
+    Assert.False(ProjectHelper.MatchWildCards(".git/HEAD", include, exclude));
+    Assert.False(ProjectHelper.MatchWildCards(".git/config", include, exclude));
+
+    // Test cases for files outside .git directory
+    Assert.True(ProjectHelper.MatchWildCards("test.vhd", include, exclude));
+    Assert.True(ProjectHelper.MatchWildCards("nice/test.vhd", include, exclude));
+    Assert.True(ProjectHelper.MatchWildCards("src/OneWare.Core/Helpers/ProjectHelper.cs", include, exclude));
+
+    // Test cases for nested directories outside .git directory
+    Assert.True(ProjectHelper.MatchWildCards("tests/OneWare.Essentials.UnitTests/ProjectHelperTests.cs", include, exclude));
+    Assert.True(ProjectHelper.MatchWildCards("docs/README.md", include, exclude));
+
+    // Test cases with multiple exclude patterns
+    var multiExclude = new[] { ".git", "node_modules" };
+    Assert.False(ProjectHelper.MatchWildCards(".git/FETCH_HEAD", include, multiExclude));
+    Assert.False(ProjectHelper.MatchWildCards("node_modules/some-package/index.js", include, multiExclude));
+
+    // Test cases with no exclude patterns
+    var noExclude = new string[] { };
+    Assert.True(ProjectHelper.MatchWildCards(".git/FETCH_HEAD", include, noExclude));
+    Assert.True(ProjectHelper.MatchWildCards("test.vhd", include, noExclude));
+}
 }
