f35c6d2085e7f5421378e80e26dc76895cc738f1
Flojo101 <florian.kaempchen@smail.th-koeln.de>
2025-02-20 17:02:50 +0100

    Add basic settings service for projects
    This service will only add settings to the project, the values of these settings need to be retrieved through other means

---
+++ src/OneWare.Essentials/Services/IProjectSettingsService.cs
@@ -0,0 +1,16 @@
+﻿using OneWare.Essentials.Models;
+
+namespace OneWare.Essentials.Services;
+
+public interface IProjectSettingsService
+{
+    public void AddProjectSetting(string key, TitledSetting projectSetting);
+    
+    public void AddProjectSetting(ProjectSetting projectSetting);
+
+    public T[] GetComboOptions<T>(string key);
+
+    public void Load(string path);
+    
+    public void Save(string path);
+}
---
+++ src/OneWare.UniversalFpgaProjectSystem/Services/ProjectSettingService.cs
@@ -0,0 +1,34 @@
+﻿using OneWare.Essentials.Models;
+using OneWare.Essentials.Services;
+
+namespace OneWare.UniversalFpgaProjectSystem.Services;
+
+public class ProjectSettingService : IProjectSettingsService
+{
+    public Dictionary<string, ProjectSetting> ProjectSettings { get; } = new();
+    
+    public void AddProjectSetting(string key, TitledSetting projectSetting)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public void AddProjectSetting(ProjectSetting projectSetting)
+    {
+        throw new NotImplementedException();
+    }
+
+    public T[] GetComboOptions<T>(string key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Load(string path)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Save(string path)
+    {
+        throw new NotImplementedException();
+    }
+}
