b6b61a6cece1a853ea65cc23900bd588dc4a456e
Hendrik Mennen <hmennen@one-ware.com>
2025-08-21 16:30:11 +0200

    progress

--- src/OneWare.CloudIntegration/ViewModels/OneWareCloudAccountFlyoutViewModel.cs
+++ src/OneWare.CloudIntegration/ViewModels/OneWareCloudAccountFlyoutViewModel.cs
@@ -19,36 +19,25 @@ public class OneWareCloudAccountFlyoutViewModel : ObservableObject
     private const string BaseUrl = "https://cloud.one-ware.com";
     private const string RegisterUrl = $"{BaseUrl}/Account/Register";
     private const string ManageAccountUrl = $"{BaseUrl}/Account/Manage";
-
-    private readonly OneWareCloudAccountSetting _setting;
     
-    private IImage? _image;
-    private string? _settingLabel;
-    private string? _email;
     private string? _urlLabel;
     private string? _url;
 
     public OneWareCloudAccountFlyoutViewModel(OneWareCloudAccountSetting setting)
     {
-        _setting = setting;
+        SettingViewModel = new OneWareCloudAccountSettingViewModel(setting);
         
-        setting.WhenValueChanged(x => x.Email)
-            .Subscribe(value => Email = value);
-        setting.WhenValueChanged(x => x.Image)
-            .Subscribe(value => Image = value);
         setting.WhenValueChanged(x => x.IsLoggedIn)
             .Subscribe(value =>
             {
                 if (value)
                 {
                     UrlLabel = "Manage your account";
-                    SettingLabel = "Logout";
                     Url = ManageAccountUrl;
                 }
                 else
                 {
                     UrlLabel = "Create an account";
-                    SettingLabel = "Login";
                     Url = RegisterUrl;
                 }
 
@@ -58,23 +47,10 @@ public class OneWareCloudAccountFlyoutViewModel : ObservableObject
             });
     }
 
+    public OneWareCloudAccountSettingViewModel SettingViewModel { get; }
+    
     public ObservableCollection<IOneWareCloudAccountFlyoutSetting> Information { get; } = [];
     
-    public string? Email
-    {
-        get => _email;
-        set => SetProperty(ref _email, value);
-    }
-    public IImage? Image
-    {
-        get => _image;
-        set => SetProperty(ref _image, value);
-    }
-    public string? SettingLabel
-    {
-        get => _settingLabel;
-        set => SetProperty(ref _settingLabel, value);
-    }
     public string? UrlLabel
     {
         get => _urlLabel;
@@ -84,33 +60,6 @@ public class OneWareCloudAccountFlyoutViewModel : ObservableObject
     {
         get => _url;
         set => SetProperty(ref _url, value);
-    }
-    
-    public Task HandleAuthAsync(Control owner)
-    {
-        if (owner is Button btn)
-            btn.Flyout?.Hide();
-
-        if (!_setting.IsLoggedIn)
-        {
-            return Dispatcher.UIThread.InvokeAsync(() => ContainerLocator.Container.Resolve<IWindowService>()
-                .ShowDialogAsync(new AuthenticateCloudView()
-                {
-                    DataContext = ContainerLocator.Container.Resolve<AuthenticateCloudViewModel>()
-                }, TopLevel.GetTopLevel(owner) as Window));
-        }
-        else
-        {
-            ContainerLocator.Container.Resolve<OneWareCloudLoginService>().Logout(Email!);
-            _ = ContainerLocator.Container.Resolve<OneWareCloudNotificationService>().DisconnectAsync();
-        
-            _setting.Value = string.Empty;
-
-            ContainerLocator.Container.Resolve<ISettingsService>()
-                .Save(ContainerLocator.Container.Resolve<IPaths>().SettingsPath);
-            
-            return Task.CompletedTask;
-        }
     }
 }
 
