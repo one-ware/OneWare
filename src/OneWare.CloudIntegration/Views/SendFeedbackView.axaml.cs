f0fba94a28ba7a3c8a7e6a7986605cb05e28666d
Matthias Schanzenbach <matthias.schanzenbach@outlook.de>
2025-09-25 16:07:10 +0200

    Added feedback dialog e.g. for bug reports and feature requests

---
+++ src/OneWare.CloudIntegration/Views/SendFeedbackView.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using OneWare.Essentials.Controls;
+
+namespace OneWare.CloudIntegration.Views;
+
+public partial class SendFeedbackView : FlexibleWindow
+{
+    public SendFeedbackView()
+    {
+        InitializeComponent();
+    }
+}
