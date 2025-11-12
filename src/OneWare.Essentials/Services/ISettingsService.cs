d8f4d2510c53572a40590e5780344f40c466dc44
Hendrik Mennen <hmennen@one-ware.com>
2025-11-12 16:22:16 +0100

    add GetSetting method to ISettingsService

--- src/OneWare.Essentials/Services/ISettingsService.cs
+++ src/OneWare.Essentials/Services/ISettingsService.cs
@@ -47,6 +47,8 @@ public interface ISettingsService
     public void RegisterSetting(string cateogory, string subCategory, string key, TitledSetting setting);
 
     public void RegisterCustom(string category, string subCategory, string key, CustomSetting customSetting);
+    
+    public Setting GetSetting(string key);
 
     public T GetSettingValue<T>(string key);
 
