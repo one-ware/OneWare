69f41f9461138ca6889a66be70de54b2faa3a14e
michael <173676957+Miresker@users.noreply.github.com>
2026-01-27 20:13:28 +0100

    Load GTKWave arguments from TestBenchContextManager

--- src/OneWare.OssCadSuiteIntegration/Tools/GtkWaveService.cs
+++ src/OneWare.OssCadSuiteIntegration/Tools/GtkWaveService.cs
@@ -1,12 +1,24 @@
 ﻿using OneWare.Essentials.Enums;
+using OneWare.Essentials.Models;
 using OneWare.Essentials.Services;
+using OneWare.UniversalFpgaProjectSystem.Context;
 
 namespace OneWare.OssCadSuiteIntegration.Tools;
 
 public class GtkWaveService(IChildProcessService childProcessService)
 {
-    public void OpenInGtkWave(string path)
+    private static readonly string[] GtkProperties = ["GtkwSaveFile", "GtkwWaveArgs"];
+
+    public async Task OpenInGtkWaveAsync(IFile file)
     {
-        childProcessService.StartWeakProcess("gtkwave", [Path.GetFileName(path)], Path.GetDirectoryName(path) ?? "");
+        var context = await TestBenchContextManager.LoadContextAsync(file);
+        List<string> args = [Path.GetFileName(file.FullPath)];
+
+        foreach (var property in GtkProperties)
+            if (context.Properties.TryGetPropertyValue(property, out var jsonNode) && jsonNode != null && jsonNode.GetValueKind() == System.Text.Json.JsonValueKind.String)            
+                args.Add(jsonNode.GetValue<string>());
+
+        // save file has to be provided as second argument without "--save"
+        childProcessService.StartWeakProcess("gtkwave", args, Path.GetDirectoryName(file.FullPath) ?? "");
     }
 }
