c572b2e369375cc36b8d21d6cc341a07e9249ae0
Hendrik Mennen <hmennen@one-ware.com>
2026-02-05 23:12:59 +0100

    cleanup some bindings

--- src/OneWare.Cyc5000/ViewModels/Cyc5000ViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿using OneWare.UniversalFpgaProjectSystem.Models;
-using OneWare.UniversalFpgaProjectSystem.ViewModels;
-
-namespace OneWare.Cyc5000.ViewModels;
-
-public class Cyc5000ViewModel(FpgaModel model) : FpgaViewModelBase(model);
