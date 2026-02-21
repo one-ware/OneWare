754c7b6a2557edfecb2f08a3cc6263248883865a
Hendrik Mennen <hmennen@one-ware.com>
2026-02-21 01:32:33 +0100

    onnxruntime installer set runtime automatically

--- src/OneWare.PackageManager/Installers/OnnxRuntimePackageInstaller.cs
+++ src/OneWare.PackageManager/Installers/OnnxRuntimePackageInstaller.cs
@@ -1,5 +1,6 @@
 using OneWare.Essentials.Enums;
 using OneWare.Essentials.PackageManager;
+using OneWare.Essentials.Services;
 
 namespace OneWare.PackageManager.Installers;
 
@@ -10,12 +11,23 @@ public class OnnxRuntimePackageInstaller : PackageInstallerBase
     public override Task<PackageInstallerResult> InstallAsync(PackageInstallContext context,
         CancellationToken cancellationToken = default)
     {
+        ContainerLocator.Container.Resolve<ISettingsService>().SetSettingValue("OnnxRuntime_SelectedRuntime",
+            Path.GetFileName(context.ExtractionPath));
+        ContainerLocator.Container.Resolve<ISettingsService>()
+            .Save(ContainerLocator.Container.Resolve<IPaths>().SettingsPath);
         return Task.FromResult(new PackageInstallerResult(PackageStatus.NeedRestart));
     }
 
     public override Task<PackageInstallerResult> RemoveAsync(PackageInstallContext context,
         CancellationToken cancellationToken = default)
     {
+        var settingsService = ContainerLocator.Container.Resolve<ISettingsService>();
+        var selectedOnnxRuntime = settingsService.GetSettingValue<string>("OnnxRuntime_SelectedRuntime");
+        if (selectedOnnxRuntime == Path.GetFileName(context.ExtractionPath))
+        {
+            settingsService.SetSettingValue("OnnxRuntime_SelectedRuntime", "onnxruntime-cpu");
+            settingsService.Save(ContainerLocator.Container.Resolve<IPaths>().SettingsPath);
+        }
         return Task.FromResult(new PackageInstallerResult(PackageStatus.NeedRestart));
     }
 }
