cba9e3aac50c0d11885b6ecb14aa3b6a0cd725aa
Sebastian Wittlich <sebastian.wittlich@th-koeln.de>
2026-05-07 10:18:51 +0200

    Showcase impl

--- src/OneWare.Essentials/ToolEngine/ToolCommand.cs
+++ src/OneWare.Essentials/ToolEngine/ToolCommand.cs
@@ -1,3 +1,4 @@
+using System.Runtime.InteropServices;
 using OneWare.Essentials.Enums;
 
 namespace OneWare.Essentials.ToolEngine;
@@ -6,7 +7,10 @@ public class ToolCommand
 {
     public required string ToolName { get; init; }
     public string? Executable { get; init; }
-    public IReadOnlyCollection<string> Arguments { get; init; } = [];
+    public IReadOnlyCollection<string> Arguments => 
+        CommandArguments.Select(x => x.GetArgument()).ToList().AsReadOnly();
+    
+    public IReadOnlyCollection<ICommandArgument> CommandArguments { get; init; } = [];
     public string WorkingDirectory { get; init; } = ".";
     public string StatusMessage { get; init; } = "Running tool...";
     public AppState State { get; init; } = AppState.Loading;
@@ -15,6 +19,22 @@ public class ToolCommand
     public Func<string, bool>? OutputHandler { get; init; }
     public Func<string, bool>? ErrorHandler { get; init; }
 
+    public void ConvertFilePaths(OSPlatform osPlatform)
+    {
+        foreach (var argument in CommandArguments)
+        {
+            if (argument is PathArgument pathArgument)
+            {
+                pathArgument.ChangeOsPath(osPlatform);
+            }
+        }
+    }
+
+    private static IReadOnlyCollection<ICommandArgument> ParseArguments(IReadOnlyCollection<string> arguments)
+    {
+        return arguments.Select(arg => new CommandArgument(arg)).Cast<ICommandArgument>().ToList();
+    }
+    
     public static ToolCommand FromShellParams(
         string path,
         IReadOnlyCollection<string> arguments,
@@ -29,7 +49,7 @@ public class ToolCommand
         {
             ToolName = Path.GetFileNameWithoutExtension(path),
             Executable = path,
-            Arguments = arguments,
+            CommandArguments = ParseArguments(arguments),
             WorkingDirectory = workingDirectory,
             StatusMessage = status,
             State = state,
@@ -37,6 +57,65 @@ public class ToolCommand
             OutputHandler = outputAction,
             ErrorHandler = errorAction
         };
+    }
+    
+    public static ToolCommand FromParams(
+        string path,
+        IReadOnlyCollection<ICommandArgument> arguments,
+        string workingDirectory,
+        string status,
+        AppState state = AppState.Loading,
+        bool showTimer = false,
+        Func<string, bool>? outputAction = null,
+        Func<string, bool>? errorAction = null)
+    {
+        return new ToolCommand
+        {
+            ToolName = Path.GetFileNameWithoutExtension(path),
+            Executable = path,
+            CommandArguments = arguments,
+            WorkingDirectory = workingDirectory,
+            StatusMessage = status,
+            State = state,
+            ShowTimer = showTimer,
+            OutputHandler = outputAction,
+            ErrorHandler = errorAction
+        };
+    }
+}
+
+public interface ICommandArgument
+{
+    string GetArgument();
+}
+
+public class CommandArgument(string argument) : ICommandArgument
+{
+    public string GetArgument()
+    {
+        return argument;
+    }
+}
+
+public class PathArgument(string path) : ICommandArgument
+{
+    private string _path = path;
+
+    public void ChangeOsPath(OSPlatform osPlatform)
+    {
+        if (osPlatform == OSPlatform.Windows)
+        {
+            _path = _path.Replace("/", "\\");
+        }
+        else
+        {
+            _path = _path.Replace("\\", "/");
+        }
+    }
+    
+    public string GetArgument()
+    {
+        return _path;
     }
 }
 
